18fcf48e4b344280414e20d68fab4df8acb078eb
Alex Sanhueza <sanhuezainc@gmail.com>
2020-09-08 18:45:59 -0700

    React Native Boilerplate

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './client/App';
+import { name as appName } from './package.json';
+
+AppRegistry.registerComponent(appName, () => App);

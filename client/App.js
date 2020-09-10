b089f9256713841171ca624f43a38da5def704f4
Alex Sanhueza <sanhuezainc@gmail.com>
2020-09-10 16:44:37 -0700

    fixed archive

--- client/App.js
+++ client/App.js
@@ -10,15 +10,15 @@ import PostFull from './PostFull.js';
 import Login from './Login';
 
 import {
-	SafeAreaView,
-	StyleSheet,
-	ScrollView,
-	View,
-	Text,
-	StatusBar,
-	Button,
-	Alert,
-	Modal,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Alert,
+  Modal,
 } from 'react-native';
 
 const RootStack = createStackNavigator();
@@ -38,49 +38,41 @@ const RootStack = createStackNavigator();
 // };
 
 const App = () => {
-	const [loggedIn, userLogin] = useState(false);
+  const [loggedIn, userLogin] = useState(true);
 
-	if (!loggedIn) {
-		return (
-			<View>
-				<Login loggedIn={loggedIn} userLogin={userLogin} />
-			</View>
-		);
-	} else {
-		return (
-			<NavigationContainer>
-				<RootStack.Navigator>
-					<RootStack.Screen
-						name="Home"
-						component={Home}
-						options={{ title: 'Embr' }}
-					/>
-					<RootStack.Screen
-						name="Menu"
-						component={Menu}
-						options={{ title: 'Menu' }}
-						loggedIn={loggedIn}
-						userLogout={userLogin}
-					/>
-					<RootStack.Screen
-						name="PostFull"
-						component={PostFull}
-						options={{ title: 'Details' }}
-					/>
-					<RootStack.Screen
-						name="Add Posting"
-						component={AddPosting}
-						options={{ title: 'New Job Posting' }}
-					/>
-					<RootStack.Screen
-						name="Archive"
-						component={Archive}
-						options={{ title: 'Archived Postings' }}
-					/>
-				</RootStack.Navigator>
-			</NavigationContainer>
-		);
-	}
+  if (!loggedIn) {
+    return (
+      <View>
+        <Login loggedIn={loggedIn} userLogin={userLogin} />
+      </View>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Screen name='Home' component={Home} options={{ title: 'Embr' }} />
+          <RootStack.Screen
+            name='Menu'
+            component={Menu}
+            options={{ title: 'Menu' }}
+            // loggedIn={loggedIn}
+            // userLogout={userLogin}
+          />
+          <RootStack.Screen name='PostFull' component={PostFull} options={{ title: 'Details' }} />
+          <RootStack.Screen
+            name='Add Posting'
+            component={AddPosting}
+            options={{ title: 'New Job Posting' }}
+          />
+          <RootStack.Screen
+            name='Archive'
+            component={Archive}
+            options={{ title: 'Archived Postings' }}
+          />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    );
+  }
 };
 
 const styles = StyleSheet.create({});
--- client/Menu.js
+++ client/Menu.js
@@ -7,40 +7,39 @@ import Home from './Home.js';
 import Login from './Login';
 
 import {
-	SafeAreaView,
-	StyleSheet,
-	ScrollView,
-	View,
-	Text,
-	StatusBar,
-	Button,
-	Alert,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Alert,
 } from 'react-native';
 
 const Menu = ({ navigation, route, loggedIn, userLogout }) => {
-	const [logOut, setLogOut] = useState(loggedIn);
+  const [logOut, setLogOut] = useState(loggedIn);
 
-	// const handleLogout = () => {
-	// 	userLogout(false);
-	// 	navigation.navigate('App');
-	// };
+  // const handleLogout = () => {
+  // 	userLogout(false);
+  // 	navigation.navigate('App');
+  // };
 
-	console.log(route.params.jobObj);
-	return (
-		<SafeAreaView style={{ marginTop: 40 }}>
-			<Button onPress={() => navigation.navigate('Home')} title="Home"></Button>
-			<Button
-				onPress={() => navigation.navigate('Archive')}
-				title="Archived Postings"
-			></Button>
-			<Button title="Stats (Coming Soon)"></Button>
-			<Button title="Profile"></Button>
-			<Button
-				title="Log Out"
-				// onPress={}
-			></Button>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView style={{ marginTop: 40 }}>
+      <Button onPress={() => navigation.navigate('Home')} title='Home'></Button>
+      <Button
+        onPress={() => navigation.navigate('Archive', { jobArray: route.params.jobArray })}
+        title='Archived Postings'
+      ></Button>
+      <Button title='Stats (Coming Soon)'></Button>
+      <Button title='Profile'></Button>
+      <Button
+        title='Log Out'
+        // onPress={}
+      ></Button>
+    </SafeAreaView>
+  );
 };
 
 export default Menu;
